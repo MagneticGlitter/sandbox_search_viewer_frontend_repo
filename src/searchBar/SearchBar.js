374552fa5a6c8a406540f61cda551e91d36b5473
MagneticGlitter <92243872+MagneticGlitter@users.noreply.github.com>
2024-01-29 18:40:08 -0500

    Initial commit

---
+++ src/searchBar/SearchBar.js
@@ -0,0 +1,116 @@
+import "../index.css";
+import {useEffect, useState} from 'react';
+// import SpeechRecogntion, { useSpeechRecognition } from 'react-speech-recognition';
+import useSpeechRecognition from "./speech";
+const SearchBar = () => {
+  const {
+    text,
+    startListening
+  } = useSpeechRecognition();
+
+    const [data, setData] = useState([]);
+    const [keywords, setKeywords] = useState("");
+
+    const handleRandomQuery = () => {
+        window.location.href = process.env.REACT_APP_API_URL + "wiki/Special:Random";
+    }
+
+    const handleSearchQuery = async (event, direction) => { 
+        try {
+            event.preventDefault();
+            const kw = keywords
+            
+
+            // Make an HTTPS request using fetch
+            const response = await fetch(process.env.REACT_APP_HOST_API + "query", 
+                {method: 'POST', // or 'PUT', 'DELETE', etc., depending on your API
+                headers: {
+                'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    keywords: kw
+                })}
+            );
+            
+            // Check if the request was successful (status code 200-299)
+            if (response.ok) {
+                // Parse the response data (assuming it's JSON in this example)
+                const result = await response.json();
+
+                // Extract all objects from the 'pages' property of the response
+                setData(result.pages.slice(0, 50))
+
+            } else {
+            // Handle error if the request was not successful
+                console.error('Error fetching data:', response.statusText);
+            }
+        } catch (error) {
+            // Handle network errors or other exceptions
+            console.error('Error fetching data:', error.message);
+        }
+    }
+    useEffect(() => {
+        console.log(text);
+        setKeywords(text)
+    }, [text]);
+    
+    return (
+<>
+  <div className="flex flex-col items-center justify-center h-screen">
+    <div className="mb-4 text-white">
+      <button type="button" onClick={handleRandomQuery}>
+        Click here for a random article
+      </button>
+    </div>
+    <div className="relative z-10 mb-2"> 
+        <button
+            type="button"
+            className="absolute top-1/3 right-2 transform -translate-y-1/2 pr-2 mt-0.5"
+            onClick={(e) => setKeywords("")}
+        >
+            <img src='./close-button-png-23.png' className="w-4" alt="Close" />
+        </button>
+        <button
+            className="absolute left-0 top-0 m-2 p-2 mt-0.5 rounded-md"
+            style={{ paddingBottom: '4px' }}
+            onClick={startListening}
+            >
+            <img
+                src="/mic.png"
+                alt="Image"
+                className="w-6 h-6"
+            />
+        </button>
+        <input
+            type="text"
+            value={keywords}
+            onChange={(e) => setKeywords(e.target.value)}
+            className="mb-4 text-white px-10 py-1 rounded-full border-4 border-input focus:outline-none focus:border-primary-main bg-main"
+        />
+        
+    </div>
+    <div className="mb-4 text-white">
+      <button type="button" onClick={(e) => handleSearchQuery(e, "")}>
+        Click icon to search
+      </button>
+    </div>
+    <div className="overflow-y-auto max-w-screen-lg">
+      {data && (
+        <div>
+          {data.map((obj, index) => (
+            <a key={index} href={process.env.REACT_APP_API_URL + "?curid=" + obj.pageid} className="card-link">
+            <div className="card bg-white overflow-hidden mb-4 p-3">
+              <h3 className="font-bold text-black">{obj.title}</h3>
+              <p className="text-black">{obj.extract}</p>
+            </div>
+          </a>
+          ))}
+        </div>
+      )}
+    </div>
+  </div>
+</>
+    );
+}
+
+export default SearchBar;
---
+++ src/searchBar/speech.js
@@ -0,0 +1,50 @@
+import { useEffect, useState } from 'react'
+let recognition;
+
+if ("webkitSpeechRecognition") {
+  recognition = new window.webkitSpeechRecognition();
+  recognition.continuous = true;
+  recognition.lang = "en-US";
+}
+
+
+const useSpeechRecognition = () => {
+    const [text, setText] = useState("");
+    const [isListening, setIsListening] = useState(false);
+
+    useEffect(() => {
+        if (!recognition) {
+            return;
+        }
+
+        recognition.onresult = (event) => {
+            console.log("onresult event: ", event);
+            setText(event.results[0][0].transcript);
+            
+            recognition.stop();
+            setIsListening(false);
+        };
+    }, []);
+
+    const startListening = () => {
+        setText("");
+        setIsListening(true);
+        recognition.start();
+    }
+    const stopListening = () => {
+        setIsListening(false);
+        recognition.stop();
+    };
+
+    const resetText = () => setText("");
+
+    return {
+        text, 
+        isListening,
+        startListening,
+        resetText,
+        hasRecognitionSupport: !!recognition,
+    };
+};
+
+export default useSpeechRecognition;
